274f52da95ada76531ebbef02a90afbaf6664ae9
denis.e <denmetall@yandex.ru>
2019-10-17 17:06:44 +0700

    done module1-task3

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovieCard from "../movie-card/movie-card.jsx";
+import PageContent from "../page-content/page-content.jsx";
+
+const App = () => {
+  return <React.Fragment>
+    <MovieCard />
+    <PageContent />
+  </React.Fragment>;
+};
+
+export default App;
