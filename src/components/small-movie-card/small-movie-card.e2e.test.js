99dbf139c70961829798a5865c6701e336d29008
denis.e <denmetall@yandex.ru>
2019-10-22 15:25:07 +0700

    remove {} in other file

--- src/components/small-movie-card/small-movie-card.e2e.test.js
+++ src/components/small-movie-card/small-movie-card.e2e.test.js
@@ -9,7 +9,7 @@ it(`SmallMovieCard click on start title is correct`, () => {
   const clickHandler = jest.fn();
 
   const welcomeScreen = shallow(<SmallMovieCard
-    movieTitle={`some title`}
+    movieTitle="some title"
     onClickTitleHandler={clickHandler}
   />);
 
