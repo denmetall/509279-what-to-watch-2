f04999db9c3374e22c702c16f0a32d1b5555bcff
denis.e <denmetall@yandex.ru>
2019-10-22 09:44:50 +0700

    remove {}

--- src/components/small-movie-card/small-movie-card.test.js
+++ src/components/small-movie-card/small-movie-card.test.js
@@ -4,7 +4,7 @@ import SmallMovieCard from "./small-movie-card.jsx";
 
 it(`Components SmallMovieCard renders correctly`, () => {
   const tree = renderer
-    .create(<SmallMovieCard movieTitle={`some title`} onClickTitleHandler={jest.fn()}/>)
+    .create(<SmallMovieCard movieTitle="some title" onClickTitleHandler={jest.fn()}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
