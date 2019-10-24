7d333add41c9611d4e549a10fbc813c0ac2fd945
denis.e <denmetall@yandex.ru>
2019-10-24 11:47:20 +0700

    done class cards list

--- src/components/movie-cards-list/movie-cards-list.jsx
+++ src/components/movie-cards-list/movie-cards-list.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SmallMovieCard from "../small-movie-card/small-movie-card.jsx";
 import PropTypes from 'prop-types';
 
-const MovieCardsList = (props) => {
-  const {films} = props;
+class MovieCardsList extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return <div className="catalog__movies-list">
-    {films.map((film) => <SmallMovieCard key={film.id} film={film} onClickTitleHandler={() => {}} onMouseOverCard={() => {}}/>)}
-  </div>;
-};
+  render() {
+    const {films} = this.props;
+
+    return <div className="catalog__movies-list">
+      {films.map((film) => <SmallMovieCard key={film.id} film={film} onClickTitleHandler={() => {}} onMouseOverCard={() => {}}/>)}
+    </div>;
+  }
+}
 
 MovieCardsList.propTypes = {
   films: PropTypes.arrayOf(PropTypes.object).isRequired
