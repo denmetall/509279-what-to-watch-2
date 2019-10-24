1b222605b269bdfc251657fd8c372904751781de
denis.e <denmetall@yandex.ru>
2019-10-24 10:25:19 +0700

    done img mocks

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 const SmallMovieCard = (props) => {
   const {film, onClickTitleHandler} = props;
+  const {name, posterImage} = film;
+
   return <article className="small-movie-card catalog__movies-card">
     <div className="small-movie-card__image">
-      <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175"/>
+      <img src={posterImage} alt="Bohemian Rhapsody" width="280" height="175"/>
     </div>
     <h3 className="small-movie-card__title">
-      <a className="small-movie-card__link" onClick={onClickTitleHandler} href="movie-page.html">{film.name}</a>
+      <a className="small-movie-card__link" onClick={onClickTitleHandler} href="movie-page.html">{name}</a>
     </h3>
   </article>;
 };
