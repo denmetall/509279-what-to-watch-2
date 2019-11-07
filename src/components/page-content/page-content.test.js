4984ca990b433288e6d32e5fd6611113f676bd16
denis.e <denmetall@yandex.ru>
2019-11-07 10:11:11 +0700

    done test correct

--- src/components/page-content/page-content.test.js
+++ src/components/page-content/page-content.test.js
@@ -3,12 +3,14 @@ import renderer from 'react-test-renderer';
 import PageContent from "./page-content.jsx";
 import filmsMock from '../../mocks/films';
 import {HashRouter as Router} from 'react-router-dom';
+import createNodeMock from '../../mocks/create-node-mock';
 
 it(`Components PageContent renders correctly`, () => {
+  const options = {createNodeMock};
   const tree = renderer
     .create(<Router>
       <PageContent films={filmsMock} onClickTitleHandler={jest.fn()}/>
-    </Router>)
+    </Router>, options)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
