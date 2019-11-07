2c531b06592ccf17d4bbaea6d3ff98d98ad38ab8
Denmetall <denmetall@yandex.ru>
2019-11-07 22:53:25 +0700

    it adds 3 components

--- src/components/movie-card-top/movie-card-top.jsx
+++ src/components/movie-card-top/movie-card-top.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import MovieNav from "../movie-nav/movie-nav.jsx";
+import MovieCardDetails from "../movie-card-details.jsx/movie-card-details.jsx";
+import MovieCardOverview from "../movie-card-overview/movie-card-overview.jsx";
+import MovieCardReviews from "../movie-card-reviews/movie-card-reviews.jsx";
 
 const MovieCardTop = (props) => {
   const {posterImage} = props;
@@ -10,60 +14,11 @@ const MovieCardTop = (props) => {
       </div>
 
       <div className="movie-card__desc">
-        <nav className="movie-nav movie-card__nav">
-          <ul className="movie-nav__list">
-            <li className="movie-nav__item">
-              <a href="#" className="movie-nav__link">Overview</a>
-            </li>
-            <li className="movie-nav__item movie-nav__item--active">
-              <a href="#" className="movie-nav__link">Details</a>
-            </li>
-            <li className="movie-nav__item">
-              <a href="#" className="movie-nav__link">Reviews</a>
-            </li>
-          </ul>
-        </nav>
+        <MovieNav/>
 
-        <div className="movie-card__text movie-card__row">
-          <div className="movie-card__text-col">
-            <p className="movie-card__details-item">
-              <strong className="movie-card__details-name">Director</strong>
-              <span className="movie-card__details-value">Wes Andreson</span>
-            </p>
-            <p className="movie-card__details-item">
-              <strong className="movie-card__details-name">Starring</strong>
-              <span className="movie-card__details-value">
-                Bill Murray, <br/>
-                Edward Norton, <br/>
-                Jude Law, <br/>
-                Willem Dafoe, <br/>
-                Saoirse Ronan, <br/>
-                Tony Revoloru, <br/>
-                Tilda Swinton, <br/>
-                Tom Wilkinson, <br/>
-                Owen Wilkinson, <br/>
-                Adrien Brody, <br/>
-                Ralph Fiennes, <br/>
-                Jeff Goldblum
-              </span>
-            </p>
-          </div>
-
-          <div className="movie-card__text-col">
-            <p className="movie-card__details-item">
-              <strong className="movie-card__details-name">Run Time</strong>
-              <span className="movie-card__details-value">1h 39m</span>
-            </p>
-            <p className="movie-card__details-item">
-              <strong className="movie-card__details-name">Genre</strong>
-              <span className="movie-card__details-value">Comedy</span>
-            </p>
-            <p className="movie-card__details-item">
-              <strong className="movie-card__details-name">Released</strong>
-              <span className="movie-card__details-value">2014</span>
-            </p>
-          </div>
-        </div>
+        <MovieCardDetails/>
+        <MovieCardOverview/>
+        <MovieCardReviews/>
       </div>
     </div>
   </div>;
