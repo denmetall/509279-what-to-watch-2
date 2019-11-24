5ae7af9d885a61ce0943e7c1ecc04851d409b9d1
Denmetall <denmetall@yandex.ru>
2019-11-24 23:17:29 +0700

    start module7-task1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import thunk from "redux-thunk";
+import {compose} from "recompose";
+import {createAPI} from './api/api';
 
 import App from "./components/app/app.jsx";
 import {reducer} from './reducer';
 import {HashRouter as Router} from 'react-router-dom';
 
 const init = () => {
+  // const api = createAPI((...args) => store.dispatch(...args));
+
   const store = createStore(
       reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      compose(
+          // applyMiddleware(thunk.withExtraArgument(api)),
+          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      )
   );
 
   ReactDOM.render(<Provider store={store}>
