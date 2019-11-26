580d1657d86d3f44879c473799c2aa5625db5f1d
Denmetall <denmetall@yandex.ru>
2019-11-26 22:52:15 +0700

    commit with errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import {createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import thunk from "redux-thunk";
 import {compose} from "recompose";
-import {createAPI} from './api/api';
+import createAPI from './api/api';
 
 import App from "./components/app/app.jsx";
-import {reducer} from './reducer';
+import {reducer, Operation} from './reducer';
 import {HashRouter as Router} from 'react-router-dom';
 
 const init = () => {
-  // const api = createAPI((...args) => store.dispatch(...args));
+  const api = createAPI((...args) => store.dispatch(...args));
 
   const store = createStore(
       reducer,
       compose(
-          // applyMiddleware(thunk.withExtraArgument(api)),
+          applyMiddleware(thunk.withExtraArgument(api)),
           window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
       )
   );
+
+  store.dispatch(Operation.getFilms());
 
   ReactDOM.render(<Provider store={store}>
     <Router>
--- src/reducer.js
+++ src/reducer.js
@@ -3,19 +3,33 @@ import {DEFAULT_FILTER} from "./utils";
 
 const initialState = {
   genre: DEFAULT_FILTER,
-  films: filmsMock,
+  films: [],
   promoFilm: filmsMock[0]
 };
 
 const ActionType = {
-  SET_FILTER_GENRE: `SET_FILTER_GENRE`
+  SET_FILTER_GENRE: `SET_FILTER_GENRE`,
+  GET_FILMS: `GET_FILMS`
 };
 
 const ActionCreator = {
   setGenreFilter: (genre) => ({
     type: ActionType.SET_FILTER_GENRE,
     payload: genre
+  }),
+  getFilms: (films) => ({
+    type: ActionType.GET_FILMS,
+    payload: films
   })
+};
+
+const Operation = {
+  getFilms: () => (dispatch, _getState, api) => {
+    return api.get(`/films`)
+      .then((response) => {
+        dispatch(ActionCreator.getFilms(response.data));
+      });
+  },
 };
 
 const reducer = (state = initialState, action) => {
@@ -23,6 +37,10 @@ const reducer = (state = initialState, action) => {
     case ActionType.SET_FILTER_GENRE:
       return Object.assign({}, state, {
         genre: action.payload
+      });
+    case ActionType.GET_FILMS:
+      return Object.assign({}, state, {
+        films: action.payload,
       });
   }
 
@@ -32,5 +50,6 @@ const reducer = (state = initialState, action) => {
 export {
   reducer,
   ActionCreator,
-  ActionType
+  ActionType,
+  Operation
 };
