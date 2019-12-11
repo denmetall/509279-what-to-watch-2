74b86e0e71703752ea791e2d1b7df31f4fc24265
Denmetall <denmetall@yandex.ru>
2019-12-12 00:51:42 +0700

    start module8-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,25 +2,19 @@ import React from 'react';
 import {Switch, Route} from "react-router-dom";
 import MovieDetails from "../movie-details/movie-details.jsx";
 import MainPage from "../main-page/main-page.jsx";
-import connect from "react-redux/es/connect/connect";
 import SignIn from "../sign-in/sign-in.jsx";
-import PropTypes from "prop-types";
-import {getAuthorizationRequired} from "../../selectors";
 import AddReview from "../add-review/add-review.jsx";
 import withFormReview from '../../hocs/with-form-review';
 
 const AddReviewWrapped = withFormReview(AddReview);
 
-const App = (props) => {
-  const {isAuthorizationRequired} = props;
-
-  if (!isAuthorizationRequired) {
-    return <SignIn />;
-  }
+const App = () => {
   return <Switch>
     <Route path="/" exact>
       <MainPage/>
     </Route>
+    <Route path="/login" exact component={SignIn}/>
+
     <Route path="/film/:id" exact render={({match}) => {
       return <MovieDetails filmId={+match.params.id}/>;
     }}
@@ -31,17 +25,4 @@ const App = (props) => {
     />
   </Switch>;
 };
-
-const mapStateToProps = (state) => {
-  return {
-    isAuthorizationRequired: getAuthorizationRequired(state)
-  };
-};
-
-App.propTypes = {
-  isAuthorizationRequired: PropTypes.bool.isRequired
-};
-
-export {App};
-
-export default connect(mapStateToProps)(App);
+export default App;
--- src/components/user-block/user-block.jsx
+++ src/components/user-block/user-block.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {getAvatar} from "../../selectors";
 import PropTypes from "prop-types";
@@ -10,7 +11,7 @@ const UserBlock = (props) => {
       (<div className="user-block__avatar">
         <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
       </div>) :
-      (<a href="sign-in.html" className="user-block__link">Sign in</a>)
+      (<Link to="/login" className="user-block__link">Sign in</Link>)
     }
   </div>;
 };
